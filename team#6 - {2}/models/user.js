44768846ae9804bddce32e8f66764d5b3ef8bb58
Keshav Agarwal <keshavagarwal1710@gmail.com>
2021-02-20 21:33:14 +0530

    add model

---
+++ team#6 - {2}/models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const user = new schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    gender:{
+        type:String,
+        required:true
+    },
+    age:{
+        
+    }
+})
