6c64e87bbb6517fe7956fbdf52776e190564d796
unknown <201951073@iiitvadodara.ac.in>
2021-02-20 23:05:07 +0530

    Container Added

---
+++ team#3-{1}/src/Container/Properties/Property/Property.js
@@ -0,0 +1,11 @@
+import React from 'react';
+const property = (props) => {
+    return (
+        <div className="property">
+            Property
+        </div>
+    )
+}
+
+export default property
+
