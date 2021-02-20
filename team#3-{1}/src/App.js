a9a81566cda6f14753c1706506fc075afcce867c
sidharth <201952234@iiitvadodara.ac.in>
2021-02-20 22:19:55 +0530

    home initialized

--- team#3-{1}/src/App.js
+++ team#3-{1}/src/App.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+
+import Home from "./Container/Home/Home.js"
+
 class App extends Component {
-  
-}
+    render () {
+      return (
+        <div>
+            <Home/>
+        </div>
+      );
+    }
+  }
 
 export default App;
