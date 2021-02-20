5f017994e1db3ca288bad08756003b93397ee248
Arsh150701 <45517849+arsh150701@users.noreply.github.com>
2021-02-21 03:54:34 +0530

    added SplashScreen

---
+++ team#26 - 5/app/src/main/java/com/example/rickrolled/SplashScreen.java
@@ -0,0 +1,17 @@
+package com.example.rickrolled;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        startActivity(new Intent(this, MainActivity.class));
+    }
+}
