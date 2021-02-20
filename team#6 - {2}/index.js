3c8522ae735c9d1546fc2aa393f7cea08092448f
nlok5923 <nlokhande5923@gmail.com>
2021-02-20 21:02:14 +0530

    boostrap problem resolved

--- team#6 - {2}/index.js
+++ team#6 - {2}/index.js
@@ -2,12 +2,19 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const mongoose = require("mongoose");
+var logger = require('morgan');
+var bodyParser = require('body-parser')
+var path = require('path')
 
 app.set("view engine","ejs");
 app.use(express.static("public"));
+app.use(bodyParser.json({ limit: '10mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.get("/",(req,res)=>{
-    res.render("landing");
+    res.render("landing",{});
 })
 
 app.listen(port,(req,res)=>{
