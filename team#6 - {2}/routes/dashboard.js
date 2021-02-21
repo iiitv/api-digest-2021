b6add44b25650743b12fb9cc400552a8ffe6e0da
Keshav Agarwal <keshavagarwal1710@gmail.com>
2021-02-21 16:49:00 +0530

    remove all unnecessary comments and logs

--- team#6 - {2}/routes/dashboard.js
+++ team#6 - {2}/routes/dashboard.js
@@ -18,11 +18,13 @@ function getId(length) {
   }
   
 
-//currently using get for testing purposes
-// const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6Imhib3k2Mjc2OEBnbWFpbC5jb20iLCJyb2xlIjoiVXNlciIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL3NpZCI6Ijg2NDkiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3ZlcnNpb24iOiIyMDAiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL2xpbWl0IjoiOTk5OTk5OTk5IiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9tZW1iZXJzaGlwIjoiUHJlbWl1bSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbGFuZ3VhZ2UiOiJlbi1nYiIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvZXhwaXJhdGlvbiI6IjIwOTktMTItMzEiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL21lbWJlcnNoaXBzdGFydCI6IjIwMjEtMDItMjAiLCJpc3MiOiJodHRwczovL3NhbmRib3gtYXV0aHNlcnZpY2UucHJpYWlkLmNoIiwiYXVkIjoiaHR0cHM6Ly9oZWFsdGhzZXJ2aWNlLnByaWFpZC5jaCIsImV4cCI6MTYxMzg5NDM0MywibmJmIjoxNjEzODg3MTQzfQ.Bsi2O4WCUhMbXDaNAKryUUm8Fvz12_6Hlq2Wr7TXXBM';
-const token ="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6Im5sb2toYW5kZTU5MjNAZ21haWwuY29tIiwicm9sZSI6IlVzZXIiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9zaWQiOiI4NjQ4IiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy92ZXJzaW9uIjoiMjAwIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9saW1pdCI6Ijk5OTk5OTk5OSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbWVtYmVyc2hpcCI6IlByZW1pdW0iLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL2xhbmd1YWdlIjoiZW4tZ2IiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL2V4cGlyYXRpb24iOiIyMDk5LTEyLTMxIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9tZW1iZXJzaGlwc3RhcnQiOiIyMDIxLTAyLTIwIiwiaXNzIjoiaHR0cHM6Ly9zYW5kYm94LWF1dGhzZXJ2aWNlLnByaWFpZC5jaCIsImF1ZCI6Imh0dHBzOi8vaGVhbHRoc2VydmljZS5wcmlhaWQuY2giLCJleHAiOjE2MTM5MDIxMzQsIm5iZiI6MTYxMzg5NDkzNH0.sQkmoD7V6aPYN-l7GoCbrrl37a5Zxvs7SS57Cx1T2v0";
+//this is keshav token.
+const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6Imhib3k2Mjc2OEBnbWFpbC5jb20iLCJyb2xlIjoiVXNlciIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL3NpZCI6Ijg2NDkiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3ZlcnNpb24iOiIyMDAiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL2xpbWl0IjoiOTk5OTk5OTk5IiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9tZW1iZXJzaGlwIjoiUHJlbWl1bSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbGFuZ3VhZ2UiOiJlbi1nYiIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvZXhwaXJhdGlvbiI6IjIwOTktMTItMzEiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL21lbWJlcnNoaXBzdGFydCI6IjIwMjEtMDItMjAiLCJpc3MiOiJodHRwczovL3NhbmRib3gtYXV0aHNlcnZpY2UucHJpYWlkLmNoIiwiYXVkIjoiaHR0cHM6Ly9oZWFsdGhzZXJ2aWNlLnByaWFpZC5jaCIsImV4cCI6MTYxMzkxMDM2MCwibmJmIjoxNjEzOTAzMTYwfQ.lzlyBNrUgCRwSXor9GrMCTsgPnzVZg_DFZBVGXQVn2s';
+
+// const token ="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6Im5sb2toYW5kZTU5MjNAZ21haWwuY29tIiwicm9sZSI6IlVzZXIiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9zaWQiOiI4NjQ4IiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy92ZXJzaW9uIjoiMjAwIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9saW1pdCI6Ijk5OTk5OTk5OSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbWVtYmVyc2hpcCI6IlByZW1pdW0iLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL2xhbmd1YWdlIjoiZW4tZ2IiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL2V4cGlyYXRpb24iOiIyMDk5LTEyLTMxIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9tZW1iZXJzaGlwc3RhcnQiOiIyMDIxLTAyLTIwIiwiaXNzIjoiaHR0cHM6Ly9zYW5kYm94LWF1dGhzZXJ2aWNlLnByaWFpZC5jaCIsImF1ZCI6Imh0dHBzOi8vaGVhbHRoc2VydmljZS5wcmlhaWQuY2giLCJleHAiOjE2MTM5MDIxMzQsIm5iZiI6MTYxMzg5NDkzNH0.sQkmoD7V6aPYN-l7GoCbrrl37a5Zxvs7SS57Cx1T2v0";
+
 router.get('/',ensureAuth, function(req, res, next) {
-  res.render('dashboard', {});
+  res.render('dashboard');
 });
 
 router.get('/generate',ensureAuth, function(req, res, next) {
@@ -33,16 +35,17 @@ router.get('/generate',ensureAuth, function(req, res, next) {
         res.render("generator",{data:body});
     });
 });
+
 //for testing purpose
 currentArticleId = 'sdsdsd'
-router.get("/product",async (req,resp)=>{
+router.get("/product",(req,resp)=>{
     //end point
     const url = 'https://nlok5923.github.io/RawData/productData.json';
-        await request(url, { json: true }, (err, res, body) => {
+    request(url, { json: true }, (err, res, body) => {
         if (err) { return console.log(err); }
         // console.log(body);
-       resp.render('products',{productInfo: body, article_id: currentArticleId})
-      });
+        resp.render('products',{productInfo: body, article_id: currentArticleId})
+    });
 })
 
 router.post('/product',async (req,res)=>{
@@ -79,33 +82,20 @@ router.get("/diagnosis",(req,res)=>{
     const url=`https://sandbox-healthservice.priaid.ch/symptoms?token=${token}${ed}`;
     request(url, { json: true }, (err, response, body) => {
         if (err) { return console.log(err); }
-        console.log(body)
         res.render("diagnosis",{data:body});
     });
 })
 
 //post request for doing diagnosis.
-var symptoms = [];
 router.post("/diagnosis",(req,res)=>{
-    symptoms = req.body.symptoms;
-    console.log(symptoms)
-    res.redirect(`/dashboard/cure/${req.body.symptom}`);
+    let symptom = req.body.symptom;
+    res.redirect(`/dashboard/cure/${symptom}`);
 })
 
 //get request for showing possbile cure
-// router.get("/cure",async(req,res)=>{
 router.get("/cure/:id",(req,res)=>{
     const id = req.params.id;
-    // let symptom_id = "";
-    // symptoms.map((id)=>{
-        // symptom_id = symptom_id + id + ","
-    // })
-    // symptom_id.slice(0,-1);
-    // console.log(symptom_id)
-    // symptom_id = symptom_id.substring(0, symptom_id.length - 1);
-    // console.log(symptom_id)
     let ed = `&symptoms=[${id}]&format=json&language=en-gb`
-    console.log(ed)
     let url=`https://sandbox-healthservice.priaid.ch/symptoms?token=${token}${ed}`;
     let symptom = '';
     const gender = res.locals.user.gender;
@@ -113,7 +103,6 @@ router.get("/cure/:id",(req,res)=>{
     request(url, { json: true }, (err, response, body) => {
         if (err) { return console.log(err); }
         symptom = body[0].Name;
-        console.log(req.body);
         ed = `&symptoms=[${id}]&gender=${gender}&year_of_birth=${year}&format=json&language=en-gb`;
         url=`https://sandbox-healthservice.priaid.ch/diagnosis?token=${token}${ed}`;
         request(url, { json: true }, (err1, response1, body1) => {
@@ -133,12 +122,9 @@ router.get("/article/:id",(req,res)=>{
         if (err) { return console.log(err); }
         issue = body[0].Name;
         var articles = await articleData.find({disease_id:id});
-        // const bestArticle = articles[0];
-        // doc[0].para.replace(/["]+/g, "'"),
         articles.map((article)=>{
             article.data.replace(/["]+/g, "'")
         })
-        console.log(articles)
         res.render("particularArticles",{issue,articles});
     });
 })
