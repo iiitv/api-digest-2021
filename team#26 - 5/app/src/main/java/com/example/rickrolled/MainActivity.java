c16a0d91b73a79940c42df6ae6936c41b646a983
Devansh <201851038@iiitvadodara.ac.in>
2021-02-21 21:45:23 +0530

    Added navigation to next page

--- team#26 - 5/app/src/main/java/com/example/rickrolled/MainActivity.java
+++ team#26 - 5/app/src/main/java/com/example/rickrolled/MainActivity.java
@@ -16,6 +16,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.material.progressindicator.LinearProgressIndicator;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -26,50 +27,89 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView charView;
     JSONObject jsonObject;
     JSONArray jsonArray;
-    Button button;
+    Button button, button2;
+    LinearProgressIndicator progressIndicator;
     private static final String TAG = "MainActivity";
-    private static final String CHAR_URL = "https://rickandmortyapi.com/api/character";
+    private String CHAR_URL = "https://rickandmortyapi.com/api/character";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button=findViewById(R.id.nextscr);
+
+        button = findViewById(R.id.rick_roll);
+        button2 = findViewById(R.id.next);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getApplicationContext(), RickrollMe.class));
             }
         });
+        progressIndicator = findViewById(R.id.progressbar);
 
-        StringRequest stringRequest=new StringRequest(Request.Method.GET, CHAR_URL,
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, CHAR_URL,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
                         try {
                             jsonObject = new JSONObject(response);
                             jsonArray = jsonObject.getJSONArray("results");
-
                         } catch (JSONException e) {
                             e.printStackTrace();
-                            Log.d(TAG, "onResponse: "+e.getMessage());
-                        }
-                        finally {
+                            Log.d(TAG, "onResponse: " + e.getMessage());
+                        } finally {
                             charView = findViewById(R.id.charView);
                             RVAdapter rva = new RVAdapter(getApplicationContext(), jsonArray);
                             charView.setAdapter(rva);
                             charView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+                            button2.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    try {
+                                        progressIndicator.setVisibility(View.VISIBLE);
+                                        CHAR_URL = jsonObject.getJSONObject("info").getString("next");
+                                        {
+                                            StringRequest stringRequest = new StringRequest(Request.Method.GET, CHAR_URL,
+                                                    new Response.Listener<String>() {
+                                                        @Override
+                                                        public void onResponse(String response) {
+                                                            try {
+                                                                jsonObject = new JSONObject(response);
+                                                                jsonArray = jsonObject.getJSONArray("results");
+                                                            } catch (JSONException e) {
+                                                                e.printStackTrace();
+                                                                Log.d(TAG, "onResponse: " + e.getMessage());
+                                                            } finally {
+                                                                charView = findViewById(R.id.charView);
+                                                                RVAdapter rva = new RVAdapter(getApplicationContext(), jsonArray);
+                                                                charView.setAdapter(rva);
+                                                                charView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+                                                                progressIndicator.setVisibility(View.GONE);
+                                                            }
+                                                        }
+                                                    },
+                                                    new Response.ErrorListener() {
+                                                        @Override
+                                                        public void onErrorResponse(VolleyError error) {
+                                                        }
+                                                    });
+                                            RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+                                            requestQueue.add(stringRequest);
+                                        }
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            });
                         }
                     }
                 },
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-
                     }
                 });
-
-        RequestQueue requestQueue= Volley.newRequestQueue(this);
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
         requestQueue.add(stringRequest);
     }
 
