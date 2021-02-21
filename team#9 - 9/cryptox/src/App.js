7c26955165b7079aa07a27919a968684d1b4dfdd
chauhanprakhar <201951116@iiitvadodara.ac.in>
2021-02-21 05:45:05 +0530

    state

--- team#9 - 9/cryptox/src/App.js
+++ team#9 - 9/cryptox/src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [percent,setPercent] = useState(0);
+  const [percent24,setPercent24] = useState(0);
+  useEffect( () => {
+    const updateinterval = setInterval( () => {
+      fetch("https://data.messari.io/api/v1/assets/bitcoin/metrics")
+      .then(res => res.json())
+      .then(
+        res => setPercent(res.data.market_data.percent_change_usd_last_1_hour))
+      },10000)
+    return () => clearInterval(updateinterval);
+  },[percent] )
+
+  console.log(percent)
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
