e6924b18815bfc044a76ed97db50f38e5bcc16f9
Naman-1234 <namankalrabhiwani54@gmail.com>
2021-02-22 01:38:30 +0530

    Commit for pull

--- team#8-{7}/travel-manager/src/Components/Map/ReactMap.jsx
+++ team#8-{7}/travel-manager/src/Components/Map/ReactMap.jsx
@@ -2,24 +2,11 @@ import ReactMapboxGl from "react-mapbox-gl";
 import ReactMapGL, { NavigationControl, Marker } from "react-map-gl";
 import markerUrl from "./icons8-marker-100.png";
 import { useState } from "react";
-const ReactMap = ({ state }) => {
-  const { userLocation } = state;
-  console.log(state);
-  console.log(userLocation);
-  const [viewPort, setViewPort] = useState({
-    longitude: userLocation.longitude,
-    latitude: userLocation.latitude,
-    height: "80vh",
-    width: "80%",
-    zoom: 10,
-    markerWidth: 2,
-  });
 import React, { Component } from "react";
-import GoogleMapReact from "google-map-react";
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
-const ReactMap = ({ state }) => {
+ const ReactMap = ({ state }) => {
   const props = {
     center: {
       lat: state.userLocation.latitude,
@@ -41,18 +28,16 @@ const ReactMap = ({ state }) => {
         onViewportChange={(viewPort) =>
           setViewPort({ ...viewPort, markerWidth: state.markerWidth + 1 })
         }
-        className="map"
-      >
+        className="map" 
+     />
         <NavigationControl style={navControlStyle} />
         <Marker
           latitude={userLocation.latitude}
           longitude={userLocation.longitude}
           offsetLeft={-20}
-          offsetTop={-10}
-        >
+          offsetTop={-10} 
+        />
           <AnyReactComponent lat={59.955413} lng={30.337844} text="My Marker" />
-        </GoogleMapReact>
-      </div>
     </div>
   );
 };
