b6bf965184554c8ac04e59c5c295c75e914f8774
Shreyansh-Mishra <201951146@iiitvadodara.ac.in>
2021-02-21 12:10:29 +0530

    Add backend for saving event details, frontend for recipients form

--- team#4-{9}/server-assets/saveEventDetails.js
+++ team#4-{9}/server-assets/saveEventDetails.js
@@ -1,12 +1,19 @@
-const express=require('express')
-
+const express = require('express')
+const User = require('../model/user.js')
+const verifyToken = require('../security/verifytoken-middleware.js')
 sendDetailsRouter=new express.Router()
-
-sendDetailsRouter.post('/createEvent',(req,res)=>{
-    console.log('fasdfsadfsdfsdf')
-    console.log(req.body)
-    res.redirect('/addRecipents/')
-
+const cookieParser = require('cookie-parser')
+sendDetailsRouter.use(cookieParser())
+sendDetailsRouter.post('/createEvent',verifyToken, async (req,res)=>{
+    data = req.body
+    let user = await User.findOne({"username": req.username})
+    if (user!=null) {
+        user.events.push({"Title": data.event, "venue": data.eventVenue, "description": data.eventDescription, "date": data.eventDate})
+        await user.save()
+        event_ = user.events[user.events.length-1]
+        res.redirect(`/addRecipents/${event_._id}`)
+    }
+    
 
 
 })
