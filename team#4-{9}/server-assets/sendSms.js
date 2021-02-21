bbda594d5e5927ba5772595ebd79acd75abbc7b3
batflarrow <dwivedia2001@gmail.com>
2021-02-21 14:42:53 +0530

    add send invite sms using twilio smsl api

---
+++ team#4-{9}/server-assets/sendSms.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+phoneNumberList = [7046051694, 7984167023]
+
+const sendMessage = async (phoneNumberList) => {
+  const promises = []
+  phoneNumberList.forEach(number => {
+    number = '+91' + number
+    console.log(number)
+    promises.push(
+      client.messages
+        .create({
+          body: 'Hey man you you doing',
+          from: process.env.PHONE_NUMBER,
+          to: number
+        })
+    )
+    // .then(message => console.log(message.sid));
+  })
+  const message = await Promise.all(promises)
+  // console.log(message[0].id)
+}
+
+sendMessage(phoneNumberList)
+
+module.exports = sendMessage
