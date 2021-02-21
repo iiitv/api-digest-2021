7b1dfdac699ec44bef8c725a1326173d5d5a2314
Devansh <201851038@iiitvadodara.ac.in>
2021-02-22 01:22:25 +0530

    Toolbar created

--- team#26 - 5/app/src/main/java/com/example/rickrolled/MainActivity.java
+++ team#26 - 5/app/src/main/java/com/example/rickrolled/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.rickrolled;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -25,10 +29,11 @@ import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
     RecyclerView charView;
     JSONObject jsonObject;
     JSONArray jsonArray;
-    Button button, buttonnext, buttonprev;
+    Button button, buttonnext, buttonprev, btn;
     LinearProgressIndicator progressIndicator;
     private static final String TAG = "MainActivity";
     private String CHAR_URL = "https://rickandmortyapi.com/api/character";
@@ -37,6 +42,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.hometoolbar);
+        setSupportActionBar(toolbar);
+
+//        btn=findViewById(R.id.family_tree);
+//        btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(MainActivity.this, "something", Toast.LENGTH_SHORT).show();
+//            }
+//        });
 
         button = findViewById(R.id.rick_roll);
         buttonnext = findViewById(R.id.next);
@@ -51,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
         getjson();
     }
 
-    public void getjson(){
+    public void getjson() {
         StringRequest stringRequest = new StringRequest(Request.Method.GET, CHAR_URL,
                 new Response.Listener<String>() {
                     @Override
@@ -74,7 +90,7 @@ public class MainActivity extends AppCompatActivity {
                                     try {
                                         progressIndicator.setVisibility(View.VISIBLE);
                                         CHAR_URL = jsonObject.getJSONObject("info").getString("next");
-                                        if(CHAR_URL!=null)
+                                        if (CHAR_URL != null)
                                             getjson();
                                         else
                                             Toast.makeText(MainActivity.this, "Doesn't exist", Toast.LENGTH_SHORT).show();
@@ -89,11 +105,10 @@ public class MainActivity extends AppCompatActivity {
                                     try {
                                         progressIndicator.setVisibility(View.VISIBLE);
                                         CHAR_URL = jsonObject.getJSONObject("info").getString("prev");
-                                        Log.d(TAG, "onClick: 123"+CHAR_URL);
-                                        if(!CHAR_URL.equals("null")){
+                                        Log.d(TAG, "onClick: 123" + CHAR_URL);
+                                        if (!CHAR_URL.equals("null")) {
                                             getjson();
-                                        }
-                                        else{
+                                        } else {
                                             Toast.makeText(MainActivity.this, "Doesn't exist", Toast.LENGTH_SHORT).show();
                                             progressIndicator.setVisibility(View.GONE);
                                         }
@@ -118,5 +133,21 @@ public class MainActivity extends AppCompatActivity {
     public void onBackPressed() {
         super.onBackPressed();
         startActivity(new Intent(this, EndSplash.class));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.home_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.family_tree) {
+            startActivity(new Intent(getApplicationContext(), TreeActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
