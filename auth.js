0d153b9ab0024edca3de756506198d07cd605342
Anuj Pillai <56990215+anuj-2001@users.noreply.github.com>
2021-02-21 16:46:57 +0530

    Update

--- auth.js
+++ auth.js
@@ -64,11 +64,66 @@ const  callexternalapi =  (callback) =>
           }
           else
           {
-                console.log(body);
+               const variabletrainstation = body.Route;
+              //  console.log(variabletrainstation);
+               const dist = variabletrainstation[variabletrainstation.length-1].Distance;
+               console.log(dist);
+
+               var lowestdistance = (dist*0.4);
+               var maximumdistance  = (dist*0.7);
+                var possiblestation = [];
+                var possiblestationtime = []; 
+                var mm=0;
+              for(var m = 0 ; m< variabletrainstation.length; m++)
+              {
+                if(variabletrainstation[m].Distance >= lowestdistance && variabletrainstation[m].Distance <= maximumdistance)
+                {
+                         possiblestation[mm]=variabletrainstation[m].StationCode;
+                         var timefirst = variabletrainstation[m].ArrivalTime;
+                         var  timesecond  = variabletrainstation[m].DepartureTime;
+
+                         var af = timefirst.split(':'); // split it at the colons
+                         var bf = timesecond.split(':');
+                         var minutesfirst = (+af[0]) * 60 + (+af[1]);
+                         var minutesecond  =  (+bf[0]) * 60 + (+bf[1]);
+
+
+
+                        possiblestationtime[mm] = (minutesecond-minutesfirst);
+                         mm++;
+                }
+              }
+            for (var inn=0; inn < mm-1; inn++)      
+              {
+              // Last i elements are already in place  
+              for ( var jn = 0; jn < mm-inn-1; jn++) 
+              { 
+                  if (possiblestationtime[jn] > possiblestationtime[jn+1])  
+                  {
+                    var temp = possiblestationtime[jn];
+                    possiblestationtime[jn] = possiblestationtime[jn+1];
+                    possiblestationtime[jn+1]=temp;
+
+
+                    var tempn = possiblestation[jn];
+                    possiblestation[jn] = possiblestation[jn+1];
+                    possiblestation[jn+1]=tempn;
+                    // swap(&arr[j], &arr[j+1]);  
+                  }
+                      
+              }
+            }
+              console.log(possiblestation);
+              console.log(possiblestationtime);
+              
+              
+
+                // console.log(body.Route[]);
           }
         })
      }
-   
+      
+
     }
 })
   
