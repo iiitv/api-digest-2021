84132e5b393dbf759f68df7a987d3fa011e7186a
chauhanprakhar <201951116@iiitvadodara.ac.in>
2021-02-21 06:32:24 +0530

    State Modification

--- team#9 - 9/cryptox/src/App.js
+++ team#9 - 9/cryptox/src/App.js
@@ -3,8 +3,9 @@ import './App.css';
 import { useEffect, useState } from 'react';
 
 function App() {
+  const [limit,setLimit] = useState();
   const [percent,setPercent] = useState(0);
-  const [percent24,setPercent24] = useState(0);
+  const [percent24,setPercent24] = useState();
   useEffect( () => {
     const updateinterval = setInterval( () => {
       fetch("https://data.messari.io/api/v1/assets/bitcoin/metrics")
@@ -14,11 +15,16 @@ function App() {
       },10000)
     return () => clearInterval(updateinterval);
   },[percent] )
-
-  console.log(percent)
+  
+  if(percent > limit){
+    console.log("hurray")
+  }
   
   return (
     <div className="App">
+      <h1>Bitcoin</h1>
+      <h1>{percent}</h1>
+      <input value={limit} onChange={e => setLimit(e.target.value)}></input>
     </div>
   );
 }
